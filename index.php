caabf9e57290d7c9160ad735c7eb01461341bad3
matheusphb <139464891+matheusphb@users.noreply.github.com>
2024-12-13 15:31:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,38 @@
 <html>
-
 <head>
-<title>Exemplo PHP</title>
+    <title>Exemplo PHP</title>
 </head>
 <body>
-
 <?php
 ini_set("display_errors", 1);
-header('Content-Type: text/html; charset=iso-8859-1');
+error_reporting(E_ALL);
+header('Content-Type: text/html; charset=utf-8');
 
+echo 'Versão Atual do PHP: ' . phpversion() . '<br>';
 
-
-echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
-
-$servername = "54.234.153.24";
-$username = "root";
-$password = "Senha123";
-$database = "meubanco";
+$servername = getenv('DB_HOST') ?: 'db';
+$username = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASS') ?: 'Senha123';
+$database = getenv('DB_NAME') ?: 'meubanco';
 
 // Criar conexão
-
-
 $link = new mysqli($servername, $username, $password, $database);
 
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+if ($link->connect_error) {
+    die("Erro de conexão: " . $link->connect_error);
 }
 
-$valor_rand1 =  rand(1, 999);
+$valor_rand1 = rand(1, 999);
 $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
-
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
-
+$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1', '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2', '$host_name')";
 
 if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
+    echo "Novo registro criado com sucesso!";
 } else {
-  echo "Error: " . $link->error;
+    echo "Erro: " . $link->error;
 }
-
 ?>
 </body>
 </html>
